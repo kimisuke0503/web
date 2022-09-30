275a1d6f57026286f4412aa5356bdcf40f7845a0
kimisuke <kimisuke0503@gmail.com>
2022-09-30 16:56:38 +0900

    materialui導入

--- src/components/common/Hamberger.tsx
+++ src/components/common/Hamberger.tsx
@@ -1,34 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Hamberger = () => {
   return (
-    <>
-      <button className="btn-menu">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <nav>
-        <ul className="sub-nav">
-          <li>
-            <Link to="/web" className="sub-nav-menu">
-              Profile
-            </Link>
-          </li>
-          <li>
-            <Link to="/web/note" className="sub-nav-menu">
-              Note
-            </Link>
-          </li>
-          <li>
-            <Link to="/web/blog" className="sub-nav-menu">
-              Blog
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <button className="btn-menu">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
   );
 };
 
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,37 +3,74 @@ import { Link } from "react-router-dom";
 import { useMedia } from "react-use";
 import Hamberger from "./Hamberger";
 
+import Button from "@mui/material/Button";
+import styled from "@emotion/styled";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import MusicVideoIcon from "@mui/icons-material/MusicVideo";
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 const Header = () => {
   const isWide = useMedia("(min-width: 600px)");
-  return isWide ? (
+
+  const MenuButton = styled(Button)({
+    backgroundColor: "#aqua",
+  });
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        // Purple and green play nicely together.
+        main: "#432",
+      },
+    },
+  });
+
+  return (
     <header>
       <Link to="/web" className="logo">
         Kimisuke Web
       </Link>
-      <ul className="main-nav">
-        <li>
-          <Link to="/web" className="main-nav-menu">
-            Profile
-          </Link>
-        </li>
-        <li>
-          <Link to="/web/note" className="main-nav-menu">
-            Note
-          </Link>
-        </li>
-        <li>
-          <Link to="/web/blog" className="main-nav-menu">
-            Blog
-          </Link>
-        </li>
-      </ul>
-    </header>
-  ) : (
-    <header>
-      <Link to="/web" className="logo">
-        Kimisuke Web
-      </Link>
-      <Hamberger />
+      {isWide ? (
+        <ul className="main-nav">
+          <ThemeProvider theme={theme}>
+            <li>
+              <MenuButton
+                size="medium"
+                href="/web"
+                variant={"text"}
+                className="main-nav-menu"
+                startIcon={<AccountBoxIcon />}
+              >
+                Profile
+              </MenuButton>
+            </li>
+            <li>
+              <Button
+                size="medium"
+                href="/web/note"
+                className="main-nav-menu"
+                startIcon={<MenuBookIcon />}
+              >
+                Note
+              </Button>
+            </li>
+            <li>
+              <Button
+                size="medium"
+                href="/web/blog"
+                className="main-nav-menu"
+                startIcon={<MusicVideoIcon />}
+              >
+                Blog
+              </Button>
+            </li>
+          </ThemeProvider>
+        </ul>
+      ) : (
+        <Hamberger />
+      )}
     </header>
   );
 };
